e41ae765692f2f2fd805fa85dad94612923f6343
Jhoisaac <jhoisaac1191@gmail.com>
2020-10-28 15:31:02 -0500

    Fixed env var value dev

--- packages/firebase_messaging/ios/Classes/Constants.m
+++ packages/firebase_messaging/ios/Classes/Constants.m
@@ -10,8 +10,8 @@
 
 @implementation Constants
 // Assign it with following structure
-NSString *const __SERVER_DOMAIN = @"amazingwork.com";
-NSString *const __CHAT_API_DOMAIN = @"https://amazingwork.com/api/chat";
+NSString *const __SERVER_DOMAIN = @"amazingwork.net";
+NSString *const __CHAT_API_DOMAIN = @"https://amazingwork.net/api/chat";
 NSString *const __SHARED_PREFERENCES_NAME = @"FlutterSharedPreferences";
 
 @end
