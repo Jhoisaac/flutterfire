9f45dc7dbc9e20f330144f80b36d9ab06fc31819
Jhoisaac <jhoisaac1191@gmail.com>
2020-10-28 19:06:33 -0500

    Fixed URL full path

--- packages/firebase_messaging/ios/Classes/Constants.m
+++ packages/firebase_messaging/ios/Classes/Constants.m
@@ -10,7 +10,7 @@
 
 @implementation Constants
 // Assign it with following structure
-NSString *const __SERVER_DOMAIN = @"amazingwork.com";
+NSString *const __SERVER_DOMAIN = @"https://amazingwork.com";
 NSString *const __CHAT_API_DOMAIN = @"https://amazingwork.com/api/chat";
 NSString *const __SHARED_PREFERENCES_NAME = @"FlutterSharedPreferences";
 
